f4ef64c455132eb67918811dfc433a85e238c877
romipro-crack <rominauwu892@gmail.com>
2026-06-09 21:20:39 -0300

    ej7

--- hola.js
+++ hola.js
@@ -8,5 +8,9 @@ let TemperaturaActual = 3.25;
 //ej4 
 let UsuarioConectado = true; 
 //ej5
-let Nota1 , Nota2 , Nota3 = 0;
+let Trimestre_1 , Trimestre_2 , Trimestre_3 = "Nota1 , Nota2 , Nota3";
 //ej6
+let materia = Literatura;
+//ej8
+let cantidadEntrada = 0;
+ cantidadEntrada = 1;
